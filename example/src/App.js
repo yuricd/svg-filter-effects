0a79fae4b972519b3d3e369c0a62e1252b5f384e
Yuri Catarino <ycatarino@localhost.localdomain>
2019-04-27 23:11:55 -0300

    Add examples

--- example/src/App.js
+++ example/src/App.js
@@ -2,18 +2,43 @@ import React, { Component } from 'react'
 import lenna from './assets/lenna.jpg'
 import Effect from 'image-effects'
 
+import './index.css';
+
 export default class App extends Component {
   render () {
     return (
-      <div>
-        <Effect 
-          image={lenna}
-          width={500}
-          height={500}
-          blur={{deviation: 0}}
-          threed={true}
-         />
-      </div>
+      <React.Fragment>
+        <h1>Image effects</h1>
+        <div id="app" className="app">
+
+          <div className="app__item">
+            <Effect 
+              image={lenna}
+              width={500}
+              height={500}
+              threed={true}
+            />
+          </div>
+
+          <div className="app__item">
+            <Effect 
+              image={lenna}
+              width={500}
+              height={500}
+              sepia={true}
+            />
+          </div>
+
+          <div className="app__item">
+            <Effect 
+              image={lenna}
+              width={500}
+              height={500}
+              blur={{deviation: 10}}
+            />
+          </div>
+        </div>
+      </React.Fragment>
     )
   }
 }
